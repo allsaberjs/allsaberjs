779ff38319a366d19a920989c2ea83bd289a4557
dengweixu <1103579820@qq.com>
2019-09-05 23:11:27 +0800

    fix scripts's order

--- scripts/release/publish-commands/publish-to-git.js
+++ scripts/release/publish-commands/publish-to-git.js
@@ -2,15 +2,15 @@ const execa = require('execa');
 const version = process.env.version
 
 ;(async () => {
+  await execa(
+    'npm',
+    ['version', version, '-m', `chore: update version with tag v${version}`],
+    { stdio: 'inherit' }
+  )
   await execa('git', ['add', '-A'], { stdio: 'inherit' })
   await execa(
     'git',
     ['commit', '-m', `build: build production v${version}`],
-    { stdio: 'inherit' }
-  )
-  await execa(
-    'npm',
-    ['version', version, '-m', `chore: update version with tag v${version}`],
     { stdio: 'inherit' }
   )
   await execa('git', ['push', 'origin', `v${version}`], { stdio: 'inherit' })
