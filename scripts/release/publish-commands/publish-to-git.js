dff1d70b5c6f30400f44680e27bd2fb72ad03f7a
dengweixu <1103579820@qq.com>
2019-09-05 23:17:12 +0800

    build: build production vundefined

--- scripts/release/publish-commands/publish-to-git.js
+++ scripts/release/publish-commands/publish-to-git.js
@@ -2,17 +2,18 @@ const execa = require('execa');
 const vernum = process.env.version
 
 ;(async () => {
+  await execa('git', ['add', '-A'], { stdio: 'inherit' })
   await execa(
-    'npm',
-    ['version', version, '-m', `chore: update version with tag v${vernum}`],
+    'git',
+    ['commit', '-m', `build: build production v${vernum}`],
     { stdio: 'inherit' }
   )
-  await execa('git', ['add', '-A'], { stdio: 'inherit' })
-  // await execa(
-  //   'git',
-  //   ['commit', '-m', `build: build production v${vernum}`],
-  //   { stdio: 'inherit' }
-  // )
+  await execa(
+    'npm',
+    ['version', vernum, '-m', `chore: update version with tag v${vernum}`],
+    { stdio: 'inherit' }
+  )
+  
   await execa('git', ['push', 'origin', `v${vernum}`], { stdio: 'inherit' })
   await execa('git', ['push', 'origin', 'master'], { stdio: 'inherit' })
   console.log('release prod end')
