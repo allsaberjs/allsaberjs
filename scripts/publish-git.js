cb273b34b4491f230ae9cb3362027501e2df7892
dengweixu <1103579820@qq.com>
2021-03-27 22:58:25 +0800

    fox

--- scripts/publish-git.js
+++ scripts/publish-git.js
@@ -7,16 +7,8 @@ const pushgit = async () => {
   const vernum = process.env.version;
   console.log('vernum:', vernum)
   await execa('git', ['add', '-A'], { stdio: 'inherit' })
-  await execa(
-    'git',
-    ['commit', '-m', `build production v${vernum}`],
-    { stdio: 'inherit' }
-  )
-  await execa(
-    'npm',
-    ['version', vernum, '-m', `chore: update version with tag v${vernum}`],
-    { stdio: 'inherit' }
-  )
+  await execa('git', ['commit', '-m', `build production v${vernum}`], { stdio: 'inherit' })
+  await execa('npm', ['version', vernum, '-m', `chore: update version with tag v${vernum}`], { stdio: 'inherit' })
   // await execa('git', ['push', 'origin', `v${vernum}`], { stdio: 'inherit' })
   // await execa('git', ['push', 'origin', 'master'], { stdio: 'inherit' })
   await execa('git', ['push'], { stdio: 'inherit' })
