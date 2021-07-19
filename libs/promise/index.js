72762d1d8b98a3b5f7c15c80be050f77104404fb
xudengwei <xudw@lianlianpay.com>
2021-07-19 15:51:32 +0800

    rec

---
+++ libs/promise/index.js
@@ -0,0 +1,21 @@
+/**
+ * promise
+ * 
+ * resolve、reject
+ * then 及链式调用
+ * all
+ * any
+ * race
+ * finally
+ * allSettled
+ * catch
+ */
+
+class Promise {
+  constructor(func){
+    
+  }
+  then(res) {
+    
+  }
+}
